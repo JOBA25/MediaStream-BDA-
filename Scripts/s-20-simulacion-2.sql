917e9d5b7e657e14f577991ae4c3baf5a3e23b39
AbeJLazaro <abrahamlazaro@comunidad.unam.mx>
2021-01-14 05:15:48 -0600

    correcciones a las simulaciones

--- Scripts/s-20-simulacion-2.sql
+++ Scripts/s-20-simulacion-2.sql
@@ -1,30 +1,30 @@
 -- @Autor					Lazaro Martinez Abraham Josue
 --								Barcenas Avelar Jorge Octavio
--- @Fecha					12/01/2021
--- @Descripcion		scriipt 2: simulación carga de datos y backup
+-- @Fecha					12/01/2022
+-- @Descripcion		script 2: simulación carga de datos y backup
 
-Prompt fecha 2021/Enero/14  05:01 hrs
-!sudo date --set "2021-01-14 05:01"
+Prompt fecha 2022/Enero/14  05:01 hrs
+!sudo date --set "2022-01-14 05:01"
 
 Prompt conectando como usuario sys
-connect sys/system as sysdba
+connect sys/system@baproyla_dedicated as sysdba
 
 WHENEVER SQLERROR exit rollback;
 
 Prompt insercion en tabla Autor
-exec p_carga_autor(100);
+exec p_carga_autor(200);
 
 Prompt insercion de usuarios (usuario, tarjeta_credito, dispositivo y cargo_mensual)
-exec p_carga_usuario(100);
+exec p_carga_usuario(500);
 
 Prompt insercion en tabla registro_reproduccion 
-exec p_carga_reproduccion(1000);
+exec p_carga_reproduccion(20000);
 
 commit;
 
 WHENEVER SQLERROR continue none;
 
-Prompt fecha 2021/Enero/15  04:59 hrs
-!sudo date --set "2021-01-15 04:56"
+Prompt fecha 2022/Enero/15  04:59 hrs
+!sudo date --set "2022-01-15 04:56"
 
 PROMPT HORA DE BACKUP NIVEL 1!!!
--- Scripts/s-23-simulacion-5.sql
+++ Scripts/s-23-simulacion-5.sql
@@ -1,30 +1,30 @@
 -- @Autor					Lazaro Martinez Abraham Josue
 --								Barcenas Avelar Jorge Octavio
--- @Fecha					12/01/2021
--- @Descripcion		scriipt 5: simulación carga de datos y backup
+-- @Fecha					12/01/2022
+-- @Descripcion		script 5: simulación carga de datos y backup
 
-Prompt fecha 2021/Enero/17  05:01 hrs
-!sudo date --set "2021-01-17 05:01"
+Prompt fecha 2022/Enero/17  05:01 hrs
+!sudo date --set "2022-01-17 05:01"
 
 Prompt conectando como usuario sys
-connect sys/system as sysdba
+connect sys/system@baproyla_dedicated as sysdba
 
 WHENEVER SQLERROR exit rollback;
 
 Prompt insercion en tabla Autor
-exec p_carga_autor(100);
+exec p_carga_autor(200);
 
 Prompt insercion de usuarios (usuario, tarjeta_credito, dispositivo y cargo_mensual)
-exec p_carga_usuario(100);
+exec p_carga_usuario(500);
 
 Prompt insercion en tabla registro_reproduccion 
-exec p_carga_reproduccion(1000);
+exec p_carga_reproduccion(20000);
 
 commit;
 
 WHENEVER SQLERROR continue none;
 
-Prompt fecha 2021/Enero/18  04:59 hrs
-!sudo date --set "2021-01-18 04:56"
+Prompt fecha 2022/Enero/18  04:59 hrs
+!sudo date --set "2022-01-18 04:56"
 
 PROMPT HORA DE BACKUP NIVEL 1!!!
