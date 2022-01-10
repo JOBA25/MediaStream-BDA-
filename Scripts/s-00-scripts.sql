335960f7e195cabf4db792acc31d11e4b87e8049
AbeJLazaro <abrahamlazaro@comunidad.unam.mx>
2022-01-10 14:58:35 -0600

    agregué consulta para calcular el tamaño de los datos

--- Scripts/s-00-scripts.sql
+++ Scripts/s-00-scripts.sql
@@ -4,3 +4,7 @@ start s-09-crea-objetos-tablas.sql
 start s-10-crea-objetos-indices.sql
 start s-11-carga-datos.sql
 start s-12-carga-blob.sql
+
+-- Para la FRA
+-- select round( (sum(bytes)/(power(1024,3))) , 2 ) as GB from v$datafile;
+-- 6.03 GB
